1807e857dcb488ea8fa84c5d6fa41393dff05c4a
19else <159753ElSe->
2020-07-08 21:15:41 +0300

    1.3. Вызов метода с аргументами. [#298788]

---
+++ src/main/java/ru/job4j/oop/Jokebox.java
@@ -0,0 +1,22 @@
+package ru.job4j.oop;
+
+public class Jokebox {
+    public void music(int position) {
+        if(position == 1) {
+            System.out.println("Пусть бегут неуклюже");
+        } else {
+            if(position==2) {
+                System.out.println("Спокойной ночи");
+            } else {
+                System.out.println("Песня не найдена");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Jokebox play = new Jokebox();
+        play.music(1);
+        play.music(2);
+        play.music(3);
+    }
+}
--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -1,17 +1,18 @@
 package ru.job4j.oop;
 
 public class Student {
-    public void music() {
-        System.out.println("tra-la-la");
+    public void music(String lirics) {
+        System.out.println("tra-la-la" + lirics);
     }
     public void song() {
         System.out.println("I believe in love ");
     }
     public static void main(String[] args) {
         Student petia = new Student();
+        String song = " : I can fly away";
             for (int index = 0; index < 3; index++) {
             petia.song();
-            petia.music();
+            petia.music(song);
             }
     }
 }
