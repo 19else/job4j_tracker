a6c2931c2dd32f95dd17295219aeaa5fe66e9713
19else <159753ElSe->
2020-07-09 19:59:26 +0300

    1.4. Вызов метод с возвращаемым типом. [#298790]

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -2,12 +2,11 @@ package ru.job4j.oop;
 
 public class DummyDic {
     public String engToRus(String eng) {
-            return "Неизвестное слово - ";
+            return "Неизвестное слово - " + eng;
     }
 
     public static void main(String[] args) {
         DummyDic speak = new DummyDic();
-        String eng = "hello";
-        System.out.println(speak.engToRus(eng) + eng);
+        System.out.println(speak.engToRus("hello"));
     }
 }
