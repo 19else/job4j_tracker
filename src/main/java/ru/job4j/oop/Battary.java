2e34f227e91dfcdbca31349e7bc9dafcf0240256
19else <159753ElSe->
2020-07-12 16:36:27 +0300

    1.7. Состояние объекта. [#298791]

--- src/main/java/ru/job4j/oop/Battary.java
+++ src/main/java/ru/job4j/oop/Battary.java
@@ -8,13 +8,13 @@ public class Battary {
     }
 
     public void exchange(Battary another) {
-        this.load = this.load + another.load;
+        this.load = this.load - another.load;
         another.load = 0;
     }
 
     public static void main(String[] args) {
         Battary fullCharge = new Battary(100);
-        Battary halfCharge = new Battary(50);
+        Battary halfCharge = new Battary(30);
         System.out.println("Battary fullcharge is " + fullCharge.load + "%" + " " + " Battary halfcharge is " + halfCharge.load + "%");
         fullCharge.exchange(halfCharge);
         System.out.println("Battary charges is " + fullCharge.load + "%" + " " + " " + "Battary halfcharge is " + halfCharge.load);
