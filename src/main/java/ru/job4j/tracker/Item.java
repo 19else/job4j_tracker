ea39114176fd899ab0993deb3ae47a95f4d03004
19else <159753ElSe->
2020-07-22 19:03:27 +0300

    2. Перегрузка конструктора. [#298797]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,7 +1,25 @@
 package ru.job4j.tracker;
 
 public class Item {
-    private int id;
+
+    public Item(){
+    }
+
+    public Item(String id) {
+    }
+
+    public Item(String id, String name){
+    }
+
+    public static void main(String[] args) {
+        Item empty = new Item();
+        Item id = new Item();
+        Item idString = new Item();
+    }
+}
+
+
+    /**private int id;
     private String name;
 
     public int getId() {
@@ -18,5 +36,6 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
-    }
-}
+    }**/
+
+
