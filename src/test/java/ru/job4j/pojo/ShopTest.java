d59f88aa9a0626000dd1533db5eedd39f28c3f50
19else <159753ElSe->
2020-08-03 19:19:37 +0300

    3. Удаление моделей из массива. [#298806]

--- src/test/java/ru/job4j/pojo/ShopTest.java
+++ src/test/java/ru/job4j/pojo/ShopTest.java
@@ -8,7 +8,7 @@ import static org.hamcrest.Matchers.is;
 public class ShopTest  {
     @Test
     public void whenLastNull() {
-        Product products[] = new Product[5];
+        Product[] products = new Product[5];
         products[0] = new Product("Beer", 100);
         products[1] = new Product("Wine", 150);
         products[2] = new Product("Whiskey", 300);
@@ -18,7 +18,7 @@ public class ShopTest  {
 
     @Test
     public void whenFirstNull() {
-        Product products[] = new Product[5];
+        Product[] products = new Product[5];
         products[1] = new Product("Rome", 400);
         int rst = Shop.indexOfNull(products);
         assertThat(rst, is(0));
@@ -26,7 +26,7 @@ public class ShopTest  {
 
     @Test
     public void whenHasNotNull() {
-        Product products[] = new Product[1];
+        Product[] products = new Product[1];
         products[0] = new Product("Vodka", 200);
         int rst = Shop.indexOfNull(products);
         assertThat(rst, is(-1));
