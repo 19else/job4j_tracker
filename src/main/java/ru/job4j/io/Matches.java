7a912672cc6b9981b7fd38ba1c25817365b1749b
19else <159753ElSe->
2020-08-17 14:46:29 +0300

    2. Scanner и чтение числа из консоли. [#298743]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -5,36 +5,29 @@ public class Matches {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
        int counter = 11;
-       int gamerNumber = 1;
-        // int player = 1;
         System.out.println("Привет! Это игра #Спички# Правила игры: ");
         System.out.println("На столе лежат 11 спичек. Два игрока по очереди берут от 1 до 3 спичек. " +
                 "Выигрывает тот, кто забрал последние спички. ");
-          boolean player = true;
-         while (player) {
-             if(player = !player) {
-                 gamerNumber++;
-             } else { gamerNumber = gamerNumber;}
+        boolean player = true;
+        String name = "";
+         while (counter > 0) {
+             name = player ? "Игрок 1" : "Игрок 2";
             System.out.println("На столе " + counter + " спичек");
                 System.out.print("Введите количество спичек : ");
                 int select = Integer.valueOf(input.nextLine());
                 if (select > 0 && select < 4) {
-                    System.out.println("Игрок " + gamerNumber + " вытащил " + select + " спичек");
+                    System.out.println(name + " вытащил " + select + " спичек");
                     counter = counter - select;
-                   // gamerNumber++;
+                    player = !player;
                 } else {
                     System.out.println("Вы ввели недопустимое значение. Повторите");
                 }
-
-             }
-
+         }
         if (counter == 0) {
-
-            System.out.println("Выиграл игрок номер " + (gamerNumber - 1) + " со счетом - ");
+            System.out.println("Выиграл игрок номер " + name + " со счетом - " );
             player = false;
         }
-
-        }
     }
+}
 
 
