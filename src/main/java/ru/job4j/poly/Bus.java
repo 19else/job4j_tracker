c218b42720efaa00c9cf4158e6621ebb86176866
19else <159753ElSe->
2020-08-20 19:06:48 +0300

    3. Интерфейс Input [#298744]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public String drive() {
+        return null;
+    }
+
+    @Override
+    public int passangers(int number) {
+        return 0;
+    }
+
+    @Override
+    public int fillUp(int liters) {
+        int price = 10;
+        return price * liters;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,7 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    String drive();
+    int passangers(int number);
+    int fillUp(int liters);
+}
