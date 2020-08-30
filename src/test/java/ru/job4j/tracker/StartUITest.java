b09f39d6b9b3a63dc9d7bd07d038841538e96fc0
19else <159753ElSe->
2020-08-30 12:11:46 +0300

    5. Input и полиморфизм. [#298746]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -3,6 +3,7 @@ package ru.job4j.tracker;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
@@ -16,5 +17,31 @@ public class StartUITest {
         Item created = tracker.findAll()[0];
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
+    }
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answer = {
+                "replaced item", String.valueOf(item.getId())
+        };
+        StartUI.replaceItem(new StubInput(answer), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("delete item");
+       // tracker.add(item);
+        tracker.delete(item.getId());
+        String[] answer = {
+                String.valueOf(item.getId())
+        };
+        StartUI.deleteItem(new StubInput(answer), tracker);
+        Item deleted = tracker.findById(item.getId());
+        assertNull(deleted);
     }
 }
