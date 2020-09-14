f432f8cddddfa2feb2b671911fcec26514c1a687
19else <159753ElSe->
2020-09-14 12:00:57 +0300

    0.5. Иерархия исключений и множественный catch. [#298764]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -21,54 +21,46 @@ public class UserStore {
 
     public static boolean validate(User user) throws UserInvalidException {
         boolean rst = true;
+        String name = user.getUserName("SergeyEEE");
+        int line = name.length();
          if (user.isValid() == false) {
             rst = false;
             throw new UserInvalidException("User " + user + " access denied");
         }
-         String name = user.getUserName("SergeyEEE");
-         int line = name.length();
         if(line < 3 ) {
-            System.out.println("Name is " + name + " must be more then 3 symbols ");
+            throw new UserInvalidException("User + " + name + " must be 3 symbols and more");
         }
         return rst;
     }
 
     public static void main(String[] args) throws UserNotFoundException {
-
             User[] users = {
                     new User("Petr", true),
                     new User("Serge", true),
                     new User("AB", true),
                     new User("Anton", false)
             };
-
             User user = findUser(users, "Serge");
-            User user1 = findUser(users, "AB");
-            User user2 = findUser(users, "Anton");
-            User user3 = findUser(users, "Kuzma");
-
-            if (validate(user)) {
+                if (validate(user)) {
                 System.out.println("This user has an access");
             }
-            try {
-                validate(user3);
-            } catch (UserNotFoundException un) {
+        try {
+            User user1 = findUser(users, "AB");
+            validate(user1);
+        } catch (UserInvalidException ui) {
+            ui.printStackTrace();
+        }
+        try {
+            User user3 = findUser(users, "Kuzma");
+            validate(user3);
+        } catch (UserNotFoundException un) {
                 un.printStackTrace();
             }
-            try {
-                validate(user2);
-            } catch (UserInvalidException ui) {
+        try {
+            User user2 = findUser(users, "Anton");
+            validate(user2);
+        } catch (UserInvalidException ui) {
                 ui.printStackTrace();
             }
-
-            validate(user1);
-
-     /**    catch (UserInvalidException e) {
-            e.printStackTrace();
-        }
-          catch (UserNotFoundException ui) {
-         ui.printStackTrace();
-         }  **/
     }
-
 }
