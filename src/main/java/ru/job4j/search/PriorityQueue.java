e338f0b046877ca09638cc0be94d92321e1b5df3
19else <159753ElSe->
2020-11-23 12:30:54 +0300

    1. Телефонный справочник на базе ArrayList [#298721]

---
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,33 @@
+package ru.job4j.search;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+    /**
+     * Метод должен вставлять в нужную позицию элемент.
+     * Позиция определяется по полю приоритет.
+     * Для вставки использовать add(int index, E value)
+     * @param task задача
+     *
+     */
+
+    public void put(Task task) {
+        int index = 0;
+        for (Task element : tasks) {
+            if (task.getPriority() > element.getPriority()) {
+                index++;
+            } else {
+                this.tasks.add(index, task);
+                break;
+
+            }
+        }
+
+        this.tasks.add(index, task);
+    }
+ //Метод take возвращает первый элемент из списка и удаляет его.
+    public Task take() {
+        return tasks.poll();
+    }
+}
