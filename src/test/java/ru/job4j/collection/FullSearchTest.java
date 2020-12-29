106717203ebaac719f236707d2dbe4ae591a2a63
19else <159753ElSe->
2020-12-29 11:27:05 +0300

    1. Уникальные задачи. [#298734]

---
+++ src/test/java/ru/job4j/collection/FullSearchTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.*;
+import java.util.Arrays;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class FullSearchTest {
+    @Test
+    public void testExtractNumber() {
+        List<Task> tasks = Arrays.asList(
+                new Task("1", "First desc"),
+                new Task("2", "Second desc"),
+                new Task("1", "First desc")
+        );
+        Set<String> expected = new HashSet<>(Arrays.asList("1", "2"));
+        assertThat(FullSearch.extractNumber(tasks), is(expected));
+    }
+}
