c524b09fd19d3bbc9d2bba3e47390b7f87221560
19else <159753ElSe->
2021-01-20 17:29:12 +0300

    2. Комбинированный компаратор. [#298718]

--- src/test/java/ru/job4j/collection/JobTest.java
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -7,65 +7,79 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.lessThan;
 
 public class JobTest {
-@Test
+    @Test
     public void whenComparatorByNameAndPriority() {
-    Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
-    int rsl =cmpNamePriority.compare(
-            new Job("TheLongName", 0),
-            new Job("ShortName", 1)
-    );
-    assertThat(rsl, lessThan(0));
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl Task", 0),
+                new Job("Fix Bug", 1)
+        );
+        System.out.println(rsl);
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorByNameAndPriorityTwo() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Bob", 1),
+                new Job("Bob", 0)
+        );
+        System.out.println(rsl);
+        assertThat(rsl, lessThan(0));
     }
 
     @Test
     public void whenComparatorAscByNameAndPriority() {
-    Comparator<Job> cmpNamePriority = new JobAscByName().thenComparing(new JobAscByPriority());
-    int rsl = cmpNamePriority.compare(
-            new Job("TheLongName", 1),
-            new Job("ShortName", 0)
+        Comparator<Job> cmpNamePriority = new JobAscByName().thenComparing(new JobAscByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("TheLongName", 1),
+                new Job("ShortName", 0)
 
-    );
-    assertThat(rsl, is(1));
+        );
+        System.out.println(rsl);
+        assertThat(rsl, greaterThan(0));
     }
+
     @Test
     public void ComparatorJobAscByName() {
-    Comparator<Job> ascByName = new JobAscByName();
-        List<Job> ascName = Arrays.asList(
+        Comparator<Job> ascByName = new JobAscByName();
+        int rsl = ascByName.compare(
                 new Job("TheLongName", 3),
                 new Job("MiddleName", 2)
-        );
-        List<Job> ascName2 = Arrays.asList(
-                new Job("SecondLongName", 2),
-                new Job("SecondMiddleName", 4),
-                new Job("SecShortName",1)
-        );
-    int rsl = ascByName.compare(
-            new Job("TheLongName", 3),
-            new Job("MiddleName", 2)
 
-    );
-    Collections.sort(ascName);
+        );
         System.out.println(rsl);
-        System.out.println(ascByName);
-    assertThat(rsl, is(ascName));
+        assertThat(rsl, greaterThan(0));
     }
 
     @Test
     public void ComparatorJobAscByPriority() {
-    Comparator<Job> ascPriority = new JobAscByPriority();
-    List<Job> priority = Arrays.asList(
-            new Job("A", 4),
-            new Job("B", 3),
-            new Job("C", 2),
-            new Job("D", 1)
-    );
+        Comparator<Job> ascPriority = new JobAscByPriority();
+        List<Job> priority = Arrays.asList(
+                new Job("A", 4),
+                new Job("B", 3),
+                new Job("C", 2),
+                new Job("D", 1)
+        );
         System.out.println(priority);
-    Collections.sort(priority, ascPriority);
+        Collections.sort(priority, ascPriority);
         System.out.println("after " + priority);
+    }
+
+    @Test
+    public void ComparatorJobAscByPriorityTwo() {
+        Comparator<Job> ascPriority = new JobAscByPriority();
+        int rsl = ascPriority.compare(
+                new Job("A", 3),
+                new Job("B", 6)
+        );
+        System.out.println(rsl);
+        assertThat(rsl, lessThan(0));
     }
 }
