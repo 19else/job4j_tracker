f108e678f22e6ffc4cecd8ccfae450e223626b88
19else <159753ElSe->
2021-03-14 16:19:18 +0300

    Доп.задача после экзамена по Коллекциям

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -1,5 +1,6 @@
 package ru.job4j.collection;
 
+import java.util.Collections;
 import java.util.HashSet;
 
 public class Article {
@@ -18,5 +19,26 @@ public class Article {
             }
         }
         return result;
+    }
+
+    public static boolean generateBy2(String origin, String line) {
+        boolean result = true;
+        HashSet<String> check = new HashSet<>();
+        String lowCaseOrigin = origin.toLowerCase().replaceAll("\\p{P}", ""); //убирает знаки препинания
+        String lowCaseLine = line.toLowerCase().replaceAll("\\p{P}", "");
+        String[] source = lowCaseOrigin.split(" ");
+        String[] lineArr = lowCaseLine.split(" ");
+        Collections.addAll(check, source);
+        for (String compare : lineArr) {
+            if (!check.contains(compare)) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generateBy2("Мама мыла раму", "рамУ мыла"));
     }
 }
