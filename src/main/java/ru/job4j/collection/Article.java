f68616794334b2fd8c0f3ae031abfbee6b36ca96
19else <159753ElSe->
2021-03-14 12:13:21 +0300

    Доп.задача после экзамена по Коллекциям

---
+++ src/main/java/ru/job4j/collection/Article.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class Article {
+    public static boolean generateBy(String origin, String line) {
+        boolean result = true;
+        String[] originText = origin.toLowerCase().split("\\!|\\,|\\.|\\;| ");
+        String[] lineText = line.toLowerCase().split("\\!|\\,|\\.|\\;| ");
+        HashSet<String> checker = new HashSet<>();
+        for (String oneWord : originText) {
+            checker.add(oneWord);
+        }
+        for (String lineCheck : lineText) {
+            if (!checker.contains(lineCheck)) {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
