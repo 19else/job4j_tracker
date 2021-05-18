89ed58a3b4c58f43947f376c031d7340389da00b
19else <159753ElSe->
2021-05-18 14:25:45 +0300

    2. Функциональный интерфейс [#298573] AttachmentSort.java

--- src/main/java/ru/job4j/lambda/AttachmentSort.java
+++ src/main/java/ru/job4j/lambda/AttachmentSort.java
@@ -12,13 +12,17 @@ public class AttachmentSort {
                 new Attachment("image 3", 13)
         );
 
-        Comparator<Attachment> comparator = new Comparator()  {
+        Comparator<Attachment> comparator = new Comparator<>()  {
             @Override
-            public int compare(Object o1, Object o2) {
-                Attachment left = (Attachment) o1;
-                Attachment right = (Attachment) o2;
-                return left.getSize() - right.getSize();
+            public int compare(Attachment o1, Attachment o2) {
+                return o1.getSize() - o2.getSize();
             }
+//            @Override
+//            public int compare(Object o1, Object o2) {
+//                Attachment left = (Attachment) o1;
+//                Attachment right = (Attachment) o2;
+//                return left.getSize() - right.getSize();
+
         };
         attachments.sort(comparator);
         System.out.println("SortBySize = " + attachments);
