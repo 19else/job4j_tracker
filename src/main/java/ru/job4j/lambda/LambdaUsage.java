11d23732316b22a96f29c3bd837c5817b040e513
19else <159753ElSe->
2021-06-02 16:33:35 +0300

    2.2. Лямбда блок [#249208]

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -6,18 +6,23 @@ import java.util.Comparator;
 public class LambdaUsage {
     public static void main(String[] args) {
         String[] names = {
-                "Donald",
-                "Tom",
-                "Benjamin",
-                "John",
+                "1. Tasks",
+                "15. Tasks",
+                "11. Tasks",
+                "3. Tasks",
         };
         Comparator<String> cmpText = (left, right) -> {
-            System.out.println("compare - " + left + " : " + right);
-            return right.compareTo(left);
+            int first = Integer.valueOf(left.substring(0, left.indexOf(".")));
+            int second = Integer.valueOf(right.substring(0, right.indexOf(".")));
+            System.out.println("compare - " + first + " : " + second);
+            return Integer.compare(first, second);
         };
         Comparator<String> cmpDescSize = (o1, o2) -> {
-            System.out.println("compare - " + o1 + " : " + o2);
-            return Integer.compare(o2.length(), o1.length());
+            int first = Integer.valueOf(o1.substring(0, o1.indexOf(".")));
+            int second = Integer.valueOf(o2.substring(0, o2.indexOf(".")));
+            System.out.println("compare - " + first + " : " + second);
+            return Integer.compare(second, first);
+
         };
         Arrays.sort(names, cmpText);
         System.out.println("Sort Ascending = " + Arrays.toString(names));
