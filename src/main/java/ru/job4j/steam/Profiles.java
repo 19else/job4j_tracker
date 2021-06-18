172132e137f45acd72ed20c736e1402c55306aba
19else <159753ElSe->
2021-06-18 17:52:11 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/steam/Profiles.java
+++ src/main/java/ru/job4j/steam/Profiles.java
@@ -9,7 +9,8 @@ public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
-             //   .sorted(Comparator.comparing(Address::getHome))
+                .sorted(Comparator.comparing(Address::getCity))
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/steam/ProfilesTest.java
+++ src/test/java/ru/job4j/steam/ProfilesTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.steam;
 
 import org.junit.Test;
+
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -11,30 +12,51 @@ public class ProfilesTest {
     @Test
     public void whenCollectIsTrue() {
         List<Address> addressList = List.of(
-                new Address("moscow", "krasnaya", 1, 2),
-                new Address("Korolev", "Kosmonavtov", 10, 11)
+                new Address("Korolev", "Kosmonavtov", 10, 11),
+                new Address("Moscow", "Krasnaya", 1, 2)
         );
         Profiles profiles = new Profiles();
         List<Profile> profileList = List.of(
-                new Profile(new Address("moscow", "krasnaya", 1, 2)),
-                new Profile(new Address("Korolev", "Kosmonavtov", 10, 11))
+                new Profile(new Address("Korolev", "Kosmonavtov", 10, 11)),
+                new Profile(new Address("Moscow", "Krasnaya", 1, 2))
         );
         List<Address> rsl = profiles.collect(profileList);
         assertThat(rsl, is(addressList));
     }
-//
-//    @Test
-//    public void whenCollectIsFalse() {
-//        List<Address> profile = List.of(
-//                new Address("moscow", "krasnaya", 1, 2),
-//                new Address("Korolev", "Kosmonavtov", 10, 11)
-//        );
-//        Profiles profiles = new Profiles();
-//        List<Profile> compareList = List.of(
-//                new Profile(new Address("moscow", "krasnaya", 1, 2)),
-//                new Profile(new Address("Korolev", "Kosmonavtov", 11, 11))
-//        );
-//        List<Address> rsl = profiles.collect(compareList);
-//        assertThat(profile, is(false));
-//    }
+
+    @Test
+    public void deleteDuplicates() {
+        List<Address> addressList = List.of(
+                new Address("Korolev", "Kosmonavtov", 10, 11),
+                new Address("Moscow", "Krasnaya", 1, 2)
+        );
+        Profiles profiles = new Profiles();
+        List<Profile> compareList = List.of(
+                new Profile(new Address("Moscow", "Krasnaya", 1, 2)),
+                new Profile(new Address("Korolev", "Kosmonavtov", 10, 11)),
+                new Profile(new Address("Moscow", "Krasnaya", 1, 2)),
+                new Profile(new Address("Korolev", "Kosmonavtov", 10, 11))
+        );
+        List<Address> rsl = profiles.collect(compareList);
+        assertThat(rsl, is(addressList));
+    }
+
+    @Test
+    public void sortedByCity() {
+        List<Address> addressList = List.of(
+                new Address("Korolev", "Kosmonavtov", 10, 11),
+                new Address("Moscow", "Krasnaya", 1, 2),
+                new Address("Omsk", "Lenina", 1, 15)
+        );
+        Profiles profiles = new Profiles();
+        List<Profile> compareList = List.of(
+                new Profile(new Address("Omsk", "Lenina", 1, 15)),
+                new Profile(new Address("Moscow", "Krasnaya", 1, 2)),
+                new Profile(new Address("Korolev", "Kosmonavtov", 10, 11)),
+                new Profile(new Address("Moscow", "Krasnaya", 1, 2)),
+                new Profile(new Address("Korolev", "Kosmonavtov", 10, 11))
+        );
+        List<Address> rsl = profiles.collect(compareList);
+        assertThat(rsl, is(addressList));
+    }
 }
