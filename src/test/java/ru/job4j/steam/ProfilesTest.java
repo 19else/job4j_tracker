a79c894a94803e75816c854aabd57cdf316be767
19else <159753ElSe->
2021-06-18 17:14:28 +0300

    2. Список адресов [#110062 #168444]

--- src/test/java/ru/job4j/steam/ProfilesTest.java
+++ src/test/java/ru/job4j/steam/ProfilesTest.java
@@ -10,17 +10,17 @@ public class ProfilesTest {
 
     @Test
     public void whenCollectIsTrue() {
-        List<Address> profile = List.of(
+        List<Address> addressList = List.of(
                 new Address("moscow", "krasnaya", 1, 2),
                 new Address("Korolev", "Kosmonavtov", 10, 11)
         );
         Profiles profiles = new Profiles();
-        List<Profile> compareList = List.of(
+        List<Profile> profileList = List.of(
                 new Profile(new Address("moscow", "krasnaya", 1, 2)),
                 new Profile(new Address("Korolev", "Kosmonavtov", 10, 11))
         );
-        List<Address> rsl = profiles.collect(compareList);
-        assertThat(profile, is(rsl));
+        List<Address> rsl = profiles.collect(profileList);
+        assertThat(rsl, is(addressList));
     }
 //
 //    @Test
