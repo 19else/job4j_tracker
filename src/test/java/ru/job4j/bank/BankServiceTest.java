a3ed15348f9f938e596715f737f9339ce119ecc5
19else <159753ElSe->
2021-07-06 10:53:34 +0300

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#24260 #173572]

--- src/test/java/ru/job4j/bank/BankServiceTest.java
+++ src/test/java/ru/job4j/bank/BankServiceTest.java
@@ -2,6 +2,9 @@ package ru.job4j.bank;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -38,5 +41,16 @@ public class BankServiceTest {
         User user = new User("", "NotFound");
         BankService bank = new BankService();
         assertNull(bank.findByPassport("456"));
+    }
+
+    @Test
+    public void whenTransferIsOk() {
+        BankService bankService = new BankService();
+        bankService.addUser(new User("1234", "Ivan"));
+        bankService.addUser(new User("4567", "Fedor"));
+        bankService.addAccount("1234", new Account("159753", 200D));
+        bankService.addAccount("4567", new Account("753159", 1000D));
+        assertTrue(bankService.transferMoney("1234", "159753",
+                "4567", "753159", 50D));
     }
 }
